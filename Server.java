ee9ca84ff774df4dea1a6c80bcf43fb428a17a95
athdot <athdot@gmail.com>
2021-07-25 18:31:41 -0400

    curse you eclipse

--- Server.java
+++ Server.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * Account - CS180 PJ04
