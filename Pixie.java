8d7bed3a589318d2c0cc6c4d3bcadd076650cbb6
jmaduafo <87540591+jmaduafo@users.noreply.github.com>
2021-07-27 02:04:07 +0100

    Pixie login

--- Pixie.java
+++ Pixie.java
@@ -2,59 +2,56 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Pixie extends JComponent implements Runnable{
-    private Client client;
 
+public class PixieAccount extends JComponent implements Runnable {
     JTextField userField;
     JTextField passwordField;
+    JTextField confirmField;
     JButton loginButton;
     JButton createButton;
-    JButton signInButton;
-    Pixie go;
-    
-    
+    JButton newCreate;
+
+    PixieAccount go;
+
+    private Client client;
+
     ActionListener actionListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == createButton) {
-                go.switchToCreate();
+            if (e.getSource() == loginButton) {
+                go.switchToLogIn();
             }
-            if (e.getSource() == signInButton) {
-                go.checkPass();
+            if (e.getSource() == newCreate) {
+                go.create();
             }
-
         }
     };
 
     // Checks the validity of username and password in login page
-    public void checkPass() {
-        String userCode = "login[" + userField.getText().toLowerCase() +
-                "," + passwordField.getText() + "]";
+    public void create() {
+        String userCode = "createAccount[" + userField.getText().toLowerCase() + "," + passwordField.getText() + "]";
         String evaluate = client.streamReader(userCode);
 
-        // If invalid, show error message
+        // If username is taken, show error message
         if (evaluate.equals("false")) {
-            JOptionPane.showMessageDialog(null, "Invalid username or password", "",
+            JOptionPane.showMessageDialog(null, "Username is taken.", "",
                     JOptionPane.ERROR_MESSAGE);
         // If valid, show Welcome page (having trouble adding Welcome class
         // once button is clicked)
-        } else {
-            Welcome welcome = new Welcome();
+        } else if (evaluate.equals("true") && passwordField.equals(confirmField)){
         }
 
     }
 
-    // Expects the screen to switch from the login page to create account page
-    // once the "Create Account" button is clicked
+    // Expects the screen to switch from the create account page to login page
+    // once the "Log In" button is clicked
     // (Having trouble doing this)
-    public void switchToCreate() {
-
+    public void switchToLogIn() {
     }
 
-    // The Log In page
     public void run() {
         /* set up new elements */
-        JFrame frame = new JFrame("Pixie");
+        JFrame frame = new JFrame("Create Account");
         JPanel panel = new JPanel();
         JPanel sidePanel = new JPanel();
         JPanel grid = new JPanel();
@@ -64,7 +61,7 @@ public class Pixie extends JComponent implements Runnable{
         frame.setLocation(450, 200);
         frame.add(panel);
 
-        // Allows more flexibility with adjusting labels, buttons, etc.
+        // Main container
         panel.setLayout(null);
 
         // Side panel (on west) design
@@ -77,8 +74,10 @@ public class Pixie extends JComponent implements Runnable{
         grid.setBackground(new Color(94, 156, 156));
 
         // Log in and create account buttons
-        loginButton = new JButton("Log In");
-        createButton = new JButton("Create Account");
+        ImageIcon icon = new ImageIcon("icons8-create-24.png");
+        ImageIcon icon2 = new ImageIcon("icons8-account-24.png");
+        createButton = new JButton(icon);
+        loginButton = new JButton(icon2);
 
         // Add buttons to grid
         grid.add(loginButton);
@@ -88,40 +87,43 @@ public class Pixie extends JComponent implements Runnable{
         sidePanel.add(grid);
 
         // Buttons and labels with main panel
-        signInButton = new JButton("Sign In");
+        newCreate = new JButton("Create Account");
         JLabel username = new JLabel("Username ");
         JLabel password = new JLabel("Password ");
-        // Adjusts movement of labels on the panel
-        username.setBounds(80, 117, 80, 30);
-        password.setBounds(80, 147, 80, 30);
+        JLabel confirm = new JLabel("Confirm Password ");
+        JLabel invalid = new JLabel("Hello");
+
+        username.setBounds(80, 97, 80, 30);
+        password.setBounds(80, 127, 80, 30);
+        confirm.setBounds(80, 157, 175, 30);
+        invalid.setBounds(80, 157, 175, 30);
 
         // Text fields for username and password
         passwordField = new JPasswordField(10);
+        confirmField = new JPasswordField(10);
         userField = new JTextField(10);
 
-        userField.setBounds(160, 125, 100, 20);
-        passwordField.setBounds(160, 155, 100, 20);
-        signInButton.setBounds(130, 200, 100, 25);
-
-        signInButton.addActionListener(actionListener);
-
+        userField.setBounds(200, 105, 135, 20);
+        passwordField.setBounds(200, 135, 135, 20);
+        confirmField.setBounds(200, 165, 135, 20);
+        newCreate.setBounds(140, 200, 135, 25);
 
         // Add all to main panel
         panel.add(username);
         panel.add(password);
+        panel.add(newCreate);
+        panel.add(confirm);
 
+        panel.add(confirmField);
         panel.add(passwordField);
         panel.add(userField);
-        panel.add(signInButton);
+
 
         // Allow elements to show
         frame.setVisible(true);
     }
 
-
     public static void main(String[] args) {
-        // Run class
-        SwingUtilities.invokeLater(new Pixie());
+        SwingUtilities.invokeLater(new PixieAccount());
     }
-
 }
